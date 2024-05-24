83991e04dbf5818f35b48fe5f0c7efc4feb29343
salmuuki <snouairah@icloud.com>
2024-05-24 10:27:49 +0200

    Implement AutonomicVehicle by Salma.

---
+++ src/AutonomicVehicle.java
@@ -0,0 +1,28 @@
+public class AutonomicVehicle {
+    private String id; //ID of the vehicle
+    private Container container; //the container currently being transported by the vehicle
+
+    public AutonomicVehicle(String id) {
+        this.id = id; //start the ID
+        this.container = null; //start the container as null
+    }
+
+    //method to load a container onto the vehicle
+    public void loadContainer(Container container) {
+        this.container = container;
+    }
+
+    //method to transport the container
+    public void transportContainer() {
+        //application for transporting the container
+        System.out.println("Transporting container with ID: " + container.getId());
+    }
+
+    public String getId() {
+        return id; //returns the ID of the vehicle
+    }
+
+    public Container getContainer() {
+        return container; //returns the container currently being transported
+    }
+}
