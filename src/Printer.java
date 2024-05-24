fab5d02719b288f0f8e9d3605c8c2c0aa6bcc0e1
salmuuki <snouairah@icloud.com>
2024-05-24 11:54:42 +0200

    Implement Printer by Elina.

---
+++ src/Printer.java
@@ -0,0 +1,11 @@
+// Informationen des Tickets und des BoardingPasses
+
+public class Printer {
+    public void printTicket(Ticket ticket) {
+        System.out.println(ticket);
+    }
+
+    public void printBoardingPass(BoardingPass boardingPass) {
+        System.out.println(boardingPass);
+    }
+}
