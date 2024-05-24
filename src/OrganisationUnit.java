43bcb5e640326ea8f6b14dcb19af26da6ba8e754
salmuuki <snouairah@icloud.com>
2024-05-24 10:40:08 +0200

    Implement Lifter by Salma.

---
+++ src/OrganisationUnit.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrganisationUnit {
+    private String name; //name of the organization unit
+    private List<FederalPoliceOfficer> officers; //list of police officers in the unit
+    private List<AutonomicVehicle> vehicles; //list of autonomic vehicles in the unit
+
+    public OrganisationUnit(String name) {
+        this.name = name; //starts the name
+        this.officers = new ArrayList<>(); //starts list of police officers
+        this.vehicles = new ArrayList<>(); //starts list of vehicles
+    }
+
+    //method to add a police officer to the unit
+    public void addOfficer(FederalPoliceOfficer officer) {
+        officers.add(officer);
+    }
+
+    //method to add a vehicle to the unit
+    public void addVehicle(AutonomicVehicle vehicle) {
+        vehicles.add(vehicle);
+    }
+
+    public String getName() {
+        return name; //returns the name of the unit
+    }
+
+    public List<FederalPoliceOfficer> getOfficers() {
+        return officers; //returns the list of police officers
+    }
+
+    public List<AutonomicVehicle> getVehicles() {
+        return vehicles; //returns the list of vehicles
+    }
+}
