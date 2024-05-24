e5c88233e490a57c3dfb3bf7b46bea2dd7f91394
Roadraudi <152898759+Roadraudi@users.noreply.github.com>
2024-05-24 12:19:29 +0200

    Implementation Container and ContainerStorage

--- src/Container.java
+++ src/Container.java
@@ -1,3 +1,19 @@
+import java.util.Stack;
+
 public class Container {
+    private Stack<Baggage> baggageStack;
+    private int maxCapacity = 45;
+
+    public Container(){
+        this.baggageStack = new Stack<>();
+    }
+
+    public boolean isEmpty() {
+        return baggageStack.isEmpty();
+    }
+
+    public boolean isFull() {
+        return baggageStack.size() == maxCapacity;
+    }
 
 }
