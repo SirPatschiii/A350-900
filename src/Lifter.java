ee92f59955ad6f375272ce9d1070512a0f93771c
salmuuki <snouairah@icloud.com>
2024-05-24 10:29:38 +0200

    Implement Lifter by Salma.

---
+++ src/Lifter.java
@@ -0,0 +1,40 @@
+public class Lifter {
+    private String id; //ID of the lifter
+    private String status; //the status of the lifter (up or down)
+
+    public Lifter(String id) {
+        this.id = id; //starts the ID
+        this.status = "down"; //starts the status as down
+    }
+
+    //method to lift up the lifter
+    public void liftUp() {
+        status = "up";
+    }
+
+    //method to lift down the lifter
+    public void liftDown() {
+        status = "down";
+    }
+
+    public String getId() {
+        return id; //returns the ID of the lifter
+    }
+
+    public String getStatus() {
+        return status; //returns the status of the lifter
+    }
+
+    //method to transfer a container to the cargo area of the airplane
+    public void transferContainer(Container container, CargoArea cargoArea, ESide side) {
+        if ("up".equals(status)) {
+            if (side == ESide.LEFT) {
+                cargoArea.addContainerLeftStorageArea(container);
+            } else if (side == ESide.RIGHT) {
+                cargoArea.addContainerRightStorageArea(container);
+            }
+        } else {
+            System.out.println("Lifter is not in the correct position.");
+        }
+    }
+}
